11060a94a600b72204b3ced9a73a68d51859216f
sam chanow <samchanow@tardis.local>
2022-11-10 09:13:12 -0500

    Added Dependency installer in bin/

---
+++ app/bin/prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "INSTALLING DEPENDENCIES"
+
+pip install streamlit
+pip install proximitypyhash
+pip install pygeohash
+pip install pandas
+pip intsall matplotlib
+pip install Bokeh
+pip install streamlit-folium
+pip install overpy
+
+echo "DONE"
