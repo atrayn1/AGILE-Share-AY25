f59f15ba7d755a822825603fda4870f74bff8fb3
sam chanow <samchanow@tardis.local>
2022-12-02 14:07:17 -0500

    PolyLine around Overpass query wrapper complete.

--- app/resources/overpassQuery.py
+++ app/resources/overpassQuery.py
@@ -19,14 +19,38 @@ def overpassNearbyQuery(latitude, longitude, range):
 #Given a list of latitude and longitudes and a range
 #Build a polygon that encompasses those points
 #Return all th named Overpass Nodes within a range from that polygon
-def overpassBoudningBoxNearbyQuery(points, range):
+#Need to be LISTS of latitude and Longitude
+#Range is in Meters
+def overpassBoundingBoxNearbyQuery(latitudes, longitudes, range):
     #custom polygon boundary. Boundaries specified as 
     # (poly:lat1 long1 lat2 long2............latN longN lat1 long1). 
     # Coordinates (lat, long) at the beginning and at the end of the 
     # poly are the same signifying that it is a closed polygon. 
     # If there is a mismatch here, query fails with an error message.
+
+    #To build the polygon I am thinking of looping through the list frontways and then backways.
+    #Effectively creating a long line
+
+    #Assuming the points are being passed in as a list
     api = overpy.Overpass()
-    pass
+
+    #Create a list of location pairs (as alist themselevs)
+    points = [[str(pair[0]), str(pair[1])] for pair in zip(latitudes, longitudes)]
+
+    #Reverse the list and then concat them together
+    #points_r = list(reversed(points))
+    #polygon = points + points_r
+    #Deconstruct the polygon pairs so that they can be joined easily for the string query
+    polyline_list = sum(points, [])
+
+    #build the query
+    query = "node(around:" + str(range) + ", " + ", ".join(polyline_list) + "); out body;"
+
+    #fetch the results
+    result = api.query(query)
+
+    return filterNodeList(results=result)
+
 
 #Given an overpass Query Result
 #Return a dataframe of all the named nodes and their lat/longs
@@ -46,10 +70,24 @@ def filterNodeList(results):
 
     return nodes
 
-df = pd.read_csv(
-  "../data/test_location_data_gh.csv"
-)
+#df = pd.read_csv(
+#  "../data/test_location_data_gh.csv"
+#)
 
 #Test Code (Single Query)
 #res = overpassNearbyQuery(df['latitude'][0], df['longitude'][0], 1000)
+#print(res)
+
+#Fuction that querys a dataframe and filters based on AdId
+#def query_adid(adid, df):
+#    if adid == '':
+#        return
+
+    #Parse the df based on advertsing id
+#    parsed_df = df.loc[df.advertiser_id == adid]
+#    return parsed_df
+
+#Test Code Polygon Query
+#filtered_df = query_adid(df['advertiser_id'][0], df)
+#res = overpassBoundingBoxNearbyQuery(filtered_df['latitude'].tolist(), filtered_df['longitude'].tolist(), 10)
 #print(res)
