f3e70526be553fac18e43d80323999b3241212ba
Ernie Son <son.ernest.b@gmail.com>
2022-12-18 16:28:15 -0500

    better dataframe construction

--- app/resources/loi.py
+++ app/resources/loi.py
@@ -2,6 +2,7 @@
 # Ernest Son
 # The algorithm for returning locations of interest
 
+import numpy as np
 import pandas as pd
 import pygeohash as gh
 from datetime import datetime as dt
@@ -36,7 +37,7 @@ def LOI(data):
     #  Algorithm Begins here  #
     ###########################
 
-    #This is the output dataframe, i.e. where we store suspicious geocodes
+    # This is the output dataframe, i.e. where we store suspicious geocodes
     data_out = pd.DataFrame(columns=['geohash', 'datetime', 'latitude', 'longitude', 'advertiser_id'])
 
     # Sort by time
@@ -72,7 +73,8 @@ def LOI(data):
     # So at this point the dataframe needs to be exactly formated the same so
     # column indices are consistent
 
-    data_values = data[['geohash', 'datetime', 'latitude', 'longitude', 'advertiser_id']].values
+    relevant_features = ['geohash', 'datetime', 'latitude', 'longitude', 'advertiser_id']
+    data_values = data[relevant_features].values
     data_size = len(data_values)
 
     # Trying to keep O(n)
@@ -104,39 +106,42 @@ def LOI(data):
         # Now that we have broken out of the loop we compare the time from
         # start_index to index
 
-        # Convert strings to datetime objects or integers or something
+        # Convert strings to datetime objects so we can compare them easily
         start_time = dt.strptime(data_values[start_index, 1], '%Y-%m-%d %H:%M:%S') 
         end_time = dt.strptime(data_values[index, 1], '%Y-%m-%d %H:%M:%S')
         time_difference = end_time - start_time
         #print('start:', start_index, 'start_index:', index)
         #print('time difference:', time_difference.total_seconds())
 
-        # TODO
-        # If this is above a certain threshhold we mark it or throw it into a
-        # new filtered dataframe
-
         # we could pretty-print data points at a location of interest with a
         # different color for the demo
 
+        # If this is above a certain threshhold we mark it or throw it into a
+        # new filtered dataframe
+
         # 3600 seconds in an hour
         if time_difference.total_seconds() > 3600:
-            print(data_values[start_index], 'sus')
+            '''
             #print(len(data_values[start_index]))
-            d_sus = pd.DataFrame(columns=['geohash', 'datetime', 'latitude', 'longitude', 'advertiser_id'])#, columns=['geohash', 'datetime', 'latitude', 'longitude', 'advertiser_id'])
+            #d_sus = pd.DataFrame(columns=['geohash', 'datetime', 'latitude', 'longitude', 'advertiser_id'])#, columns=['geohash', 'datetime', 'latitude', 'longitude', 'advertiser_id'])
             #d = dict((['geohash', 'datetime', 'latitude', 'longitude', 'advertiser_id'], data_values[start_index].flatten()))
             #d_sus = pd.DataFrame(d)
 
-            #I know this is very wonky but the array shape was being weird
-            #This does technically work but lets find a better solution
+            # I know this is very wonky but the array shape was being weird
+            # This does technically work but lets find a better solution
             d_sus['geohash'] = [data_values[start_index, 0]]
             d_sus['datetime'] = [data_values[start_index, 1]]
             d_sus['latitude'] = [data_values[start_index, 2]]
             d_sus['longitude'] = [data_values[start_index, 3]]
             d_sus['advertiser_id'] = [data_values[start_index, 4]]
+            '''
+
+            # A better solution
+            d_sus = pd.DataFrame(np.atleast_2d(data_values[start_index]), columns=relevant_features)
             
             data_out = pd.concat([data_out, d_sus]) #Add this row to the out dataframe
         
-    #HERE TODO we need to look for repeated visits i.e. visits on multiple days
+    # HERE TODO we need to look for repeated visits i.e. visits on multiple days
     #I am thinking we have a dictionary with the geohashes and when we see a geohash
     #we add it as key of dictionary where value is the timestamp
     #Then we can check time distances (more than 16 hours i.e. multiple visits) and still keep O(n)
@@ -151,8 +156,9 @@ def LOI(data):
             end_time = dt.strptime(data_values[index, 1], '%Y-%m-%d %H:%M:%S')
             time_difference = end_time - start_time
             if time_difference.total_seconds() > 3600 * 12: #12 hours
-                #add to data_out
-                print(data_values[start_index], 'sus big time')
+                # add to data_out
+                '''
+                #print(data_values[start_index], 'sus big time')
                 #print(len(data_values[start_index]))
                 d_sus = pd.DataFrame(columns=['geohash', 'datetime', 'latitude', 'longitude', 'advertiser_id'])#, columns=['geohash', 'datetime', 'latitude', 'longitude', 'advertiser_id'])
                 #d = dict((['geohash', 'datetime', 'latitude', 'longitude', 'advertiser_id'], data_values[start_index].flatten()))
@@ -165,17 +171,20 @@ def LOI(data):
                 d_sus['latitude'] = [data_values[index, 2]]
                 d_sus['longitude'] = [data_values[index, 3]]
                 d_sus['advertiser_id'] = [data_values[index, 4]]
+                '''
+                d_sus = pd.DataFrame(np.atleast_2d(data_values[start_index]), columns=relevant_features)
             
-                data_out = pd.concat([data_out, d_sus]) #Add this row to the out dataframe
+                data_out = pd.concat([data_out, d_sus]) # Add this row to the out dataframe
         
         geohash_dict[data_values[index, 0]] = data_values[index, 1]
 
     
-    #Remove duplicate geohashes so we limit the size of LOI list
+    # Remove duplicate geohashes so we limit the size of LOI list
     return data_out.drop_duplicates(subset=['geohash'])
 
 # testing
-df = pd.read_csv("../data/_54aa7153-1546-ce0d-5dc9-aa9e8e371f00_weeklong_gh.csv")
+#df = pd.read_csv("../data/_54aa7153-1546-ce0d-5dc9-aa9e8e371f00_weeklong_gh.csv")
+df = pd.read_csv("../data/one_id.csv")
 loi_dataframe = LOI(df)
 
 print(loi_dataframe)
