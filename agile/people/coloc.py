fa197902651575fcafdffe27eed194b58c7dcd14
Ernie Son <son.ernest.b@gmail.com>
2022-12-29 12:18:24 -0500

    pandas indexing is janky

--- agile/people/coloc.py
+++ agile/people/coloc.py
@@ -4,19 +4,23 @@
 import numpy as np
 import pandas as pd
 import pygeohash as gh
-from datetime import datetime as dt
+from datetime import datetime, timedelta
 
 # ASSUMPTIONS
-#   data is already geohashed to sufficient precision
+#   - data is already geohashed to sufficient precision
+#   - locations of interest are from the same data set
 # INPUT
 #   data:
 #     dataframe containing all of the data
 #   lois:
-#     dataframe of just locations of interest
+#     dataframe of locations of interest for a single advertiser ID
+#   timerange:
+#     range in HOURS to match timeframes of datapoints
+#     ex. "1" means within 1 hour before or within 1 hour after an LOI timestamp
 # OUTPUT
 #   data_out:
 #     a dataframe containing co-located devices
-def colocations(data, lois, precision, debug=False) -> pd.DataFrame:
+def colocations(data, lois, timerange, debug=False) -> pd.DataFrame:
 
     # This is the output dataframe, i.e. where we store suspicious data points.
     relevant_features = ['geohash', 'datetime', 'latitude', 'longitude', 'advertiser_id']
@@ -39,30 +43,32 @@ def colocations(data, lois, precision, debug=False) -> pd.DataFrame:
 
     # We ensure that our geohashing is of sufficient precision. We don't want to
     # be too precise or else every data point will have its own geohash.
-    #data["geohash"] = data.apply(lambda d : gh.encode(d.latitude, d.longitude, precision=precision), axis=1)
+    #data["geohash"] = data.apply(lambda d : gh.encode(d.latitude, d.longitude, precision=10), axis=1)
 
     # 1)
     # From the main data array, grab rows that have a geohash that is also found
     # within the locations of interest dataframe. Pandas should make this easy.
     loi_geohashes = lois['geohash'].unique()
-    data_filtered = data[data['geohash'].isin(loi_geohashes)]
+    filtered = data[data['geohash'].isin(loi_geohashes)]
 
     # 2)
     # Remove data points that come from the same advertiser_id as the LOI data
     # that we're working with.
     loi_adid = lois['advertiser_id'][0]
-    data_filtered = data_filtered[data_filtered['advertiser_id'] != loi_adid]
-
-    if debug:
-        print("FILTERED DATA:")
-        print(data_filtered)
-        print()
+    filtered = filtered[filtered['advertiser_id'] != loi_adid]
 
     # TODO
     # 3)
     # From the filtered data points, are they there at the same time? Does the
     # timestamp associated with a given data point fall within a given range
     # around the first and last timestamp of the given location of interest?
+    timerange = timedelta(hours=timerange)
+    #filtered = filtered[(filtered['geohash'] == lois['geohash']) & (filtered['datetime'] < (lois['datetime'] + timerange)) & (filtered['datetime'] > (lois['datetime'] - timerange))]
+
+    if debug:
+        print("FILTERED DATA:")
+        print(filtered)
+        print()
 
     # Return the suspicious data points
     #return data_out
@@ -70,4 +76,4 @@ def colocations(data, lois, precision, debug=False) -> pd.DataFrame:
 # testing
 df = pd.read_csv("../../data/weeklong_gh.csv")
 locations = pd.read_csv("../../data/lois.csv")
-colocations(data=df, lois=locations, precision=10, debug=True)
+colocations(data=df, lois=locations, timerange=1, debug=True)
