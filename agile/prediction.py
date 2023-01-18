8c0c0a73374c486dd2a37ed694ce32e5a227adaa
Ernie Son <son.ernest.b@gmail.com>
2023-01-18 10:38:52 -0500

    more robust weighting based on timestamp and distance thresholds

--- agile/prediction.py
+++ agile/prediction.py
@@ -30,7 +30,6 @@ def geo_distance(lat1, lon1, lat2, lon2):
 # Data is a dataframe that contains at least ad_id, datetime, lat, long, and timestamp needs to be string
 # Speed is the cutoff speed (km/hr)
 # The Dataframe must be sorted by time!!!
-#TODO SAM
 def speed_filter(data, speed) -> pd.DataFrame:
     data['date'] = pd.to_datetime(data['datetime'])
     data['timediff'] = data['date'].diff(-1) * -1 #to offset reverse diff
@@ -41,7 +40,7 @@ def speed_filter(data, speed) -> pd.DataFrame:
     data['next_longitude'] = data['longitude'].shift(-1)
     #Throw the dataframe into an apply that passes everyhting needed to geo_distance
     data['distance'] = data.apply(lambda row : geo_distance(row.latitude, row.longitude, 
-                                                row.next_latitude, row.next_longitude), axis=1)
+                                               row.next_latitude, row.next_longitude), axis=1)
     #Calculate speed
     data['speed'] = data['distance'] / data['timediff']
 
@@ -52,28 +51,28 @@ def speed_filter(data, speed) -> pd.DataFrame:
 
 # Creates 'weight' feature in input dataframes
 # The Dataframe must be sorted by time!!!
-#TODO SAM
 def weighting(data) -> pd.DataFrame:
-    #Give every row a weight equal to the number of seconds before the next row
+    # Give every row a weight equal to the number of seconds before the next row
     data['date'] = pd.to_datetime(data['datetime'])
     data['timediff'] = data['date'].diff(-1) * -1
-    #Convert timedelta to seconds
+    # Convert timedelta to seconds
     data['timediff'] = data['timediff'].apply(lambda d : d.total_seconds())
 
-    # Normalize the timediff into weights between (0 and 1) using standard min max normalization
-    td_min = data['timediff'].min()
-    td_max = data['timediff'].max()
+    # Find sampling rate of data
+    sampling_rate = data.timediff.median()
 
-    data['weight'] = data['timediff'].apply(lambda td : ((td-td_min) / (td_max - td_min)))
-
+    # Calculate the weights depending on distances between datapoints
+    data['weights'] = (data.timediff / sampling_rate)
+    # threshold is in km
+    # gamma
+    threshold = 0.05
+    mask = data.distance <= threshold
+    data['weights'] = data['weights'].where(mask, other=1)
     return data
 
 # Returns optimal hyperparameters for DBSCAN clustering algorithm
-#TODO ERNIE
 def optimize(data):
     pass
-
-# TODO stop here
 
 # Cluster and return top X clusters
 # Top clusters determined by summation of weights
@@ -100,10 +99,10 @@ def pol_accuracy(prediction_data, gold_labels):
 #lon2 = -76.487589
 
 #print(geo_distance(lat1, lon1, lat2, lon2))
-df = pd.read_csv("../data/demo_2023-01-11.csv")
+df = pd.read_csv("../data/test.csv")
 df.sort_values(by=['datetime'], inplace=True)
 df = speed_filter(df, 120)
 df = weighting(df)
-df.to_csv('sample_time_filter.csv')
+df.to_csv('sam.csv')
 
 print(df.head())
