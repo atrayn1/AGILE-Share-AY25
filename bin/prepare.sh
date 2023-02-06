90624380baa0d6daefad3fd16f3e35c01e4f3e03
sam chanow <samchanow@tardis.local>
2023-02-06 08:38:48 -0500

    Updated prepare.sh to reflect current list of dependencies. Also fixed a type.

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -6,12 +6,13 @@ pip install streamlit
 pip install proximitypyhash
 pip install pygeohash
 pip install pandas
-pip intsall matplotlib
+pip install matplotlib
 pip install Bokeh
 pip install streamlit-folium
 pip install overpy
 pip install geopy
-pip install scikit-learn
+pip install scikit-learn==1.0.2
 pip install fpdf2
+pip install networkx
 
 echo "DONE"
