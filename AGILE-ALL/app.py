0228ab76e75fe56626c774f7c26e585f39c1723b
anuj-sirsikar <m255838@usna.edu>
2025-03-27 11:38:15 -0400

    buttons good to go

--- AGILE-ALL/app.py
+++ AGILE-ALL/app.py
@@ -878,7 +878,6 @@ elif nav_bar == 'Graph':
     with overview_c:
         if 'graph' in st.session_state:
             graph = st.session_state.graph
-            graph.top_adids.discard(None)
             topAdids = sorted(graph.top_adids)
             for adid in topAdids:
                 if st.button(f"Expand {adid}"):
